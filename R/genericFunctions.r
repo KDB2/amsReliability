239735007f792a4091aa7ab1a40131296bfa3799
KDB <KDB2@users.noreply.github.com>
2016-01-14 21:10:21 +0100

    Fix axis.ticks.margin being depreciated by ggplot2.

--- R/genericFunctions.r
+++ R/genericFunctions.r
@@ -42,7 +42,6 @@
 ################################################################################
 
 
-
 CalculProbability <- function(Probability, Scale="Lognormal")
 # Given a vector Probability of probabilities, the function calculates
 # the correspondence in standard deviations for the Lognormal case.
@@ -201,9 +200,9 @@ CreateGraph <- function(ExpDataTable, ModelDataTable, ConfidenceDataTable, Title
     #Box around the conditions in legend
     Graph <- Graph + theme(legend.key = element_rect(fill="gray90", colour = "black", linetype=0))
     # Label/ticks size
-    Graph <- Graph + theme(axis.text.x = element_text(face="bold", size=16))
-    Graph <- Graph + theme(axis.text.y = element_text(size=16))
-    Graph <- Graph + theme(axis.ticks.length = unit(-.25, "cm"), axis.ticks.margin=unit(0.4, "cm"))
+    Graph <- Graph + theme(axis.text.x = element_text(face="bold", size=16, margin=margin(0.4,0,0,0, "cm")))
+    Graph <- Graph + theme(axis.text.y = element_text(size=16, margin=margin(0,0.4,0,0.2, "cm")))
+    Graph <- Graph + theme(axis.ticks.length = unit(-0.25, "cm"))#, axis.ticks.margin = unit(0.4, "cm")) #Depreciated see margin above.
     # Add a title
     Graph <- Graph + ggtitle(Title)
     Graph <- Graph + theme(plot.title = element_text(face="bold", size=18))
